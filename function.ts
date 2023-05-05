22487e456b2c620f5d77698b99651a2b76d7c9d1
Devesh Yadav <ydevesh136@gmail.com>
2023-05-05 14:32:37 +0530

    changes

--- function.ts
+++ function.ts
@@ -8,8 +8,8 @@ console.log(c);
 var func=(num:number):void|number=>{       //important
     console.log(num);
 }
-var func2=(num:number):number=>{
-    return num*3;
+var func2=(num:number):number[]=>{
+    return [num*3];
 }
 
 //using never makes it clear that the function will never complete normally and can help prevent errors in your code.
